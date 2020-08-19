dfdfa2e79f9b6f88775c4063f7238e9793600e67
naitik2314 <shahnaitik2000@gmail.com>
2020-08-20 00:32:02 +0530

    Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyGoogleMap());
+  runApp(MaterialApp(
+    home: MyGoogleMap(),
+  ));
 }
 
 class MyGoogleMap extends StatefulWidget {
