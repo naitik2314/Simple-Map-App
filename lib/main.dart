694eeb5b382a7ade5549fcafb6d6b9672c09ede1
naitik2314 <shahnaitik2000@gmail.com>
2020-08-21 22:20:09 +0530

    Maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'naitik.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class _MyGoogleMapState extends State<MyGoogleMap> {
       appBar: AppBar(
         title: Text('Maps by Naitik'),
       ),
-      body: Text("Body"),
+      body: GoogleMap(initialCameraPosition: null),
       floatingActionButton: FloatingActionButton(
         onPressed: naitik,
         child: Icon(Icons.add),
